0c900ac63ebabf61200f5541d4dce70c7d99cdbc
pjomehri <pjomehri@gmail.com>
2019-11-10 14:34:10 -0500

    Updates

---
+++ precache-manifest.86abc5595ddc842648f436b63eb7d845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6a217f31cc2bc94031b2a8959134cc",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2c7f6eae0f9b51233512",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "f5135225d2aebdf917b5",
+    "url": "/robofriends/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2c7f6eae0f9b51233512",
+    "url": "/robofriends/static/js/2.09385f19.chunk.js"
+  },
+  {
+    "revision": "f5135225d2aebdf917b5",
+    "url": "/robofriends/static/js/main.90895588.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
